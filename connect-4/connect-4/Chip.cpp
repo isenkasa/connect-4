f3af4693a7c51a4c5f7ff2a805e77afd2d77abb7
Isen <isenkasa@gmail.com>
2021-01-08 21:22:43 -0600

    Improvements and Documentation

--- connect-4/connect-4/Chip.cpp
+++ connect-4/connect-4/Chip.cpp
@@ -1,25 +1,46 @@
 #include "Chip.h"
 
+// Sets the color of the chip to BLANK
 Chip::Chip()
 {
 	color = BLANK;
 }
 
+/**
+* Overloaded constructor for Chip
+* 
+* @param color to be passed in
+*/
 Chip::Chip(char color)
 {
 	this->color = color;
 }
 
+/**
+* Setter for Chip Color
+* 
+* @param color to be passed in
+*/
 void Chip::setColor(char color)
 {
 	this->color = color;
 }
 
+/**
+* Getter for Chip Color
+* 
+* @return color of the Chip
+*/
 int Chip::getColor() const
 {
 	return color;
 }
 
+/**
+* Converts the Chip Color to string
+* 
+* @return string formatted color
+*/
 string Chip::toString()
 {
 	stringstream stream;
--- connect-4/connect-4/Player.cpp
+++ connect-4/connect-4/Player.cpp
@@ -1,31 +1,57 @@
 #include"Player.h"
 
+// Constructor to initialize the name and chip color for Player
 Player::Player()
 {
 	name = "Player X";
 	color = RED;
 }
 
+/**
+* Getter for Player Name
+*
+* @return the Player Name
+*/
 string Player::getName() const
 {
 	return name;
 }
 
+/**
+* Getter for Player Color 
+*
+* @return the Player Color
+*/
 char Player::getColor() const
 {
 	return color;
 }
 
+/**
+* Setter for Player Name
+* 
+* @param input - the name to be passed in
+*/
 void Player::setName(string input)
 {
 	name = input;
 }
 
+/**
+* Setter for Player Color
+* 
+* @param input - the color to be passed in
+*/
 void Player::setColor(char input)
 {
 	color = input;
 }
 
+/**
+* Converts the player name and color to string
+* 
+* @return the player name and color
+*/
 string Player::toString()
 {
 	string returnString;
